ef379df47769aab1763a8fc00f5b1a644078afb7
Nguyen Van Nam <boychauphong2001@gmail.com>
2021-08-16 16:06:58 +0700

    done

--- client/src/contexts/Constands.js
+++ client/src/contexts/Constands.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'something'
+export const API_URL =
+    process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:3000/api'
+        : 'https://shrouded-wildwood-64222.herokuapp.com/api'
